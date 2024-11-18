53df46751e807d52b220c6214817f90cd810b031
Wahbi <wahbi182@gmail.com>
2024-11-18 16:53:24 -0500

    Store saving

--- src/controllers/storeController.js
+++ src/controllers/storeController.js
@@ -3,7 +3,7 @@ const storeModel = require('../models/storeModels');
 const getAllStores = async (req, res) => {
     const { userId } = req;  // Extract userId from the request
     try {
-        const stores = await storeModel.getStoresByUserId(userId);
+        const stores = await storeModel.getAllStoresByUserID(userId);
         return res.status(200).json(stores);
     } catch (error) {
         console.error("Error retrieving stores:", error);
@@ -13,11 +13,10 @@ const getAllStores = async (req, res) => {
 
 // Create an order linked to the authenticated user
 const addStoreConnection = async (req, res) => {
-    const { userId } = req;
-    const storeData = req.body;
+    const { userId, store_name, store_domain, store_access_key } = req.body;
 
     try {
-        const store = await storeModel.addStoreConnection(userId, storeData);
+        const store = await storeModel.addStoreConnection(userId, store_name, store_domain, store_access_key);
         return res.status(201).json(store);
     } catch (error) {
         console.error("Error creating order:", error);
--- src/models/storeModels.js
+++ src/models/storeModels.js
@@ -1,24 +1,23 @@
 const db = require('../config/db.config');
 
 // Retrieve orders for a specific user
-const getAllStores = async (userId) => {
+const getAllStoresByUserID = async (userId) => {
     const result = await db.query('SELECT * FROM stores WHERE user_id = $1', [userId]);
     return result.rows;
 };
 
 // Create a new order linked to a specific user
-const addStoreConnection = async (userId, orderData) => {
-    const { total_cost, store_name, store_domain } = orderData;
+const addStoreConnection = async (userId, store_name, store_domain, store_access_key) => {
     const result = await db.query(
         `INSERT INTO stores (user_id, store_name, store_domain)
          VALUES ($1, $2, $3, $4)
          RETURNING *`,
-        [userId, total_cost, store_name, store_domain]
+        [userId, store_name, store_domain, store_access_key]
     );
     return result.rows[0];
 };
 
 module.exports = {
-    getAllStores,
+    getAllStoresByUserID,
     addStoreConnection
 };
